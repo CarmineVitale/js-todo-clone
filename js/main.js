3ee5a3a2877f5443336d9fa1b053b02db12d223c
CarmineVitale <carmine.vitale90@gmail.com>
2020-04-16 17:31:38 +0200

    Richiesta esercizio completata

--- js/main.js
+++ js/main.js
@@ -1,4 +1,50 @@
 $(document).ready(function () {
-    console.log('ready');
-    
-});
+   var coseDaFare = [
+       'Completare esercizio',
+       'Pushare su GitHub',
+       'Rivedere Slide e video lezione'
+   ];
+   var list = $('ul');
+   var newInput = $('input');
+
+   for (var i = 0; i < coseDaFare.length; i++) {
+       //clono il template
+       var elemLists = $('.template li').clone();
+        //aggiungo trsto dinamaico al template
+        elemLists.prepend(coseDaFare[i]);
+        //aggiungo cllone alla lista in APP
+        list.append(elemLists);
+   }
+   //Rimuovo un todo dalla lista
+   $('body').on('click' , 'li i' ,function () { 
+       $(this).parent().remove();
+       
+       
+   });
+   //Aggiungo attività attraverso INPUT
+   newInput.keyup(function(event){
+    //    console.log(event.keyCode);
+       
+    if (event.keyCode == 13) {
+        var textInput = newInput.val().trim();
+       
+        if (textInput !== '') {
+            var newElem = $('.template li').clone();
+            newElem.prepend(textInput);
+            list.append(newElem);
+             //pulisco input
+            newInput.val('');
+        }
+
+        
+      
+
+
+    }
+   });
+   //Line through su list item
+   $('body').on('click', 'li', function () {
+       $(this).toggleClass('completed')
+   })
+
+}); //<----End Ready
